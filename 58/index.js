d9514b16f9e3eb2b4374f2e0433a371f630da8a2
impradip <pradipyadav2438317@gmail.com>
2026-02-13 09:39:12 +0530

    add folder 58 functions

---
+++ 58/index.js
@@ -0,0 +1,22 @@
+// simple function
+
+function funcn1(a,b){
+    console.log(a+b)
+}
+funcn1(2,3)// calling funcn1
+
+
+// function for concatenation of sttring 
+function str(name){
+    console.log("hellow mittar! "+name)
+}
+str("Ranjeet")// calling str function
+
+//arrow function 
+// function func2(a,b){
+//     console.log (a+b)
+// } 
+const func2=(a,b)=>{ // const func2= (a,b) => a+b;
+    console.log (a+b)
+} 
+func2(3,9)
