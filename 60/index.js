64d7d73fbb984374d5926d7a7cbddb2aae034065
impradip <pradipyadav2438317@gmail.com>
2026-02-15 10:04:11 +0530

    add folder 60

---
+++ 60/index.js
@@ -0,0 +1,28 @@
+console.log('this is lecture 60 for string')
+
+// accessing each character of a string 
+let a="pradip";
+console.log(a[0]);
+console.log(a[4]);
+
+// finding lenght of the given string 
+console.log (a.length);
+
+//concatenation of string 
+let name="Pradip yadav ";
+console.log (" hii this is " + name)
+console.log(`hii this is ${name}`); // this method is called template litterals 
+
+//some inbuilt functions of js
+let b="Pradip Yadav";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(0,5));
+
+//replace function
+console.log(b.replace("Pradip","impradip"));
+
+//contenation of string usning concat function and + operater
+console.log(b.concat("NJMS"))
+console.log(b+a);
